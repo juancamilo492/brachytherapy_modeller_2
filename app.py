dd99e3f4ff96041426d05a69b879f43695935419
juancamilo492 <133069709+juancamilo492@users.noreply.github.com>
2025-04-27 13:12:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -248,19 +248,21 @@ if uploaded_file:
             max_coronal = image_3d.shape[1] - 1
             max_sagittal = image_3d.shape[2] - 1
 
+
             st.sidebar.markdown("#### Selección de cortes")
              
-             axial_idx = st.sidebar.number_input(
+            axial_idx = st.sidebar.number_input(
                  "Corte axial (Z)", min_value=0, max_value=max_axial, value=max_axial // 2, step=1
              )
              
-             coronal_idx = st.sidebar.number_input(
+            coronal_idx = st.sidebar.number_input(
                  "Corte coronal (Y)", min_value=0, max_value=max_coronal, value=max_coronal // 2, step=1
              )
              
-             sagittal_idx = st.sidebar.number_input(
+            sagittal_idx = st.sidebar.number_input(
                  "Corte sagital (X)", min_value=0, max_value=max_sagittal, value=max_sagittal // 2, step=1
              )
+             
              
             
             # Opciones de ventana predeterminadas
